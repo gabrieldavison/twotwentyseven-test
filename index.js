d1e11d03855a3c997b96e783d8721c5ea7af2a9c
Gabriel <mail@gabrieldavison.me.uk>
2021-02-19 17:12:15 +0000

    Add hero carousel

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ $(document).ready(function () {
     arrows: false,
     autoplay: true,
   });
+  $(".hero__carousel").slick({
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    fade: true,
+  });
 });
